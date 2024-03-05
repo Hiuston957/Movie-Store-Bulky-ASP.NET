e4c7227a159e552f6d428fefbd8e64b02577c749
Luke <blackeyesblackbear@gmail.com>
2024-03-05 21:49:41 +0100

    Add table to category view contains list of all catrgories

--- BulkyWeb/Migrations/20240305201759_SeedCatrgoryTable.Designer.cs
+++ BulkyWeb/Migrations/20240305201759_SeedCatrgoryTable.Designer.cs
@@ -3,6 +3,7 @@ using BulkyWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BulkyWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240305201759_SeedCatrgoryTable")]
+    partial class SeedCatrgoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
