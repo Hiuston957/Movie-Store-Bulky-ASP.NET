e1328261b161aa0634829bba0bf8c78cec735f44
Luke <blackeyesblackbear@gmail.com>
2024-03-06 22:01:43 +0100

    change  Bootswatch style, add Create category view and inplemend it

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -18,6 +18,20 @@ namespace BulkyWeb.Controllers
         }
 
 
+        public IActionResult Create() 
+        { 
+        return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
 
     }
 }
